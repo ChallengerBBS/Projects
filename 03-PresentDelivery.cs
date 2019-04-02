ea8e7db8bf9ffc2d5d068edb4a1874caa96f5f6c
ChallengerBBS <49166600+ChallengerBBS@users.noreply.github.com>
2019-04-02 09:58:09 +0300

    Add files via upload

---
+++ 03-PresentDelivery.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace _03_PresentDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var houseAndPresents = Console.ReadLine().Split('@').Select(int.Parse).ToList();
+            string command;
+            int currentPosition = 0;
+            int houseAccomplished = 0;
+            while ((command=Console.ReadLine())!="Merry Xmas!")
+            {
+                
+                string[] commandLine = command.Split();
+                int jumpLenght = int.Parse(commandLine[1]);
+                currentPosition += jumpLenght;
+                while(currentPosition>=houseAndPresents.Count)
+                {
+                    currentPosition -= houseAndPresents.Count;
+                }
+                if(houseAndPresents[currentPosition]==0)
+                {
+                    Console.WriteLine($"House {currentPosition} will have a Merry Christmas.");
+                }
+                else if (houseAndPresents[currentPosition] != 0)
+                {
+                    houseAndPresents[currentPosition] -= 2;
+                    if (houseAndPresents[currentPosition] <= 0)
+                        houseAccomplished++;
+                }
+               
+                if (houseAccomplished == houseAndPresents.Count)
+                    break;
+
+            }
+            if (houseAccomplished == houseAndPresents.Count)
+            {
+                Console.WriteLine($"Santa's last position was {currentPosition}.");
+                Console.WriteLine("Mission was successful.");
+
+            }
+            else
+            {
+                Console.WriteLine($"Santa's last position was {currentPosition}.");
+                Console.WriteLine($"Santa has failed {houseAndPresents.Count - houseAccomplished} houses.");
+            }
+
+        }
+    }
+}
---
+++ 10LadyBugs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _10LadyBugs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
