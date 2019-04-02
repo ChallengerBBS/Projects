6c00040c8f4041bde87bc529bf1619b227f8a523
ChallengerBBS <49166600+ChallengerBBS@users.noreply.github.com>
2019-04-02 10:22:45 +0300

    Add files via upload
    Adding some extra files from my student portfolio.

---
+++ Zad4Exam.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 5364;
+            int day = 1;
+            while (day <= 5)
+            {
+                string command = Console.ReadLine();
+                if (command == "END") break;
+                if (command == "Yes") day++;
+                if (day > 5) break;
+                int m = int.Parse(Console.ReadLine());
+                sum += m;
+                if (sum >= 8848) break;
+            }
+            if (sum >= 8848) Console.WriteLine("Goal reached for {0} days!", day);
+            else if (sum < 8848)
+            {
+                Console.WriteLine("Failed!");
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
---
+++ Zad5Exam.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Equal_sum_Left_Right_position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            for (int i = num1; i <= num2; i++)
+            {
+                int currentNum = i;
+                int leftSum = 0;
+                int rightSum = 0;
+                int midNum = 0;
+                for (int r = 1; r <= 2; r++)
+                {
+                    rightSum += currentNum % 10;
+                    currentNum /= 10;
+                }
+                midNum += currentNum % 10;
+                currentNum /= 10;
+                for (int l = 4; l <= 5; l++)
+                {
+                    leftSum += currentNum % 10;
+                    currentNum /= 10;
+                }
+                if (rightSum > leftSum)
+                {
+                    leftSum += midNum;
+                }
+                else if (leftSum > rightSum)
+                {
+                    rightSum += midNum;
+                }
+
+                if (rightSum == leftSum)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
