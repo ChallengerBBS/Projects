252a428a9d9bed5ec447a683b14defc0aeede862
Sevdalin Vasilev <49166600+ChallengerBBS@users.noreply.github.com>
2019-12-18 17:45:38 +0200

    Add files via upload

---
+++ C# EntityFramework/BestPracticesArchitecture/PetStory.Data/Configuration/ToyOrderConfiguration.cs
@@ -0,0 +1,23 @@
+﻿namespace PetStore.Data.Configuration
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Models;
+    public class ToyOrderConfiguration : IEntityTypeConfiguration<ToyOrder>
+    {
+        public void Configure(EntityTypeBuilder<ToyOrder> toyOrder)
+        {
+            toyOrder.HasKey(fo => new { fo.OrderId, fo.ToyId });
+
+            toyOrder.HasOne(fo => fo.Toy)
+                     .WithMany(fo => fo.Orders)
+                     .HasForeignKey(fo => fo.ToyId)
+                     .OnDelete(DeleteBehavior.Restrict);
+
+            toyOrder.HasOne(fo => fo.Order)
+                    .WithMany(fo => fo.Toys)
+                    .HasForeignKey(fo => fo.OrderId)
+                    .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
