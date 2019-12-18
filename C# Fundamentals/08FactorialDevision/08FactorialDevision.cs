87d8888ffb3cd8af86878309502a48fb1ed006ed
Sevdalin Vasilev <49166600+ChallengerBBS@users.noreply.github.com>
2019-12-18 16:06:46 +0200

    Add files via upload

---
+++ C# Fundamentals/08FactorialDevision/08FactorialDevision.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08FactorialDevision
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{FactorialSumOfNumbers(firstNumber, secondNumber):F2}");
+        }
+
+        private static double FactorialSumOfNumbers(int firstNumber, int secondNumber)
+        {
+            double firstFactorial = 1;
+            double secondFactorial = 1;
+            for (int i = 2; i <= firstNumber; i++)
+            {
+                firstFactorial *= i;
+            }
+            for (int i = 2; i <= secondNumber; i++)
+            {
+                secondFactorial *= i;
+            }
+            return firstFactorial / secondFactorial;
+        }
+    }
+}
