1be4f5e0581c51ecadb2b9d1b3cded26c7c17899
Sevdalin Vasilev <predator187@abv.bg>
2020-05-26 23:31:16 +0300

    Added candidate input form

--- C# ASP.NET Core MVC/MyFirstAspNetCoreApp/MyFirstAspNetCoreApp/Controllers/TestController.cs
+++ C# ASP.NET Core MVC/MyFirstAspNetCoreApp/MyFirstAspNetCoreApp/Controllers/TestController.cs
@@ -1,43 +1,58 @@
 ﻿namespace MyFirstAspNetCoreApp.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
+    using System;
     using System.ComponentModel.DataAnnotations;
 
     public class Names
     {
         [Required]
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
     }
 
     public class TestInputModel
     {
-        public int Id { get; set; }
 
         [Required]
         public Names Names { get; set; }
 
         [Required]
         [MinLength(3)]
+        [Display(Name = "University")]
         public string University { get; set; }
 
         [Required]
         [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(10,MinimumLength=10)]
-        [RegularExpression("[0-9]{10}", ErrorMessage ="Invalid EGN !")]
+        [StringLength(10, MinimumLength = 10)]
+        [RegularExpression("[0-9]{10}", ErrorMessage = "Invalid EGN !")]
+        [Display(Name = "EGN")]
         public string Egn { get; set; }
 
-        public int[] Years { get; set; }
+        [Display(Name = "Date of birth")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
 
-        public int Year { get; set; }
+        [Display(Name ="Years of experience")]
+        public int YearsOfExperience { get; set; }
     }
 
     public class TestController : Controller
     {
+
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
         public IActionResult Index(TestInputModel input)
         {
             if (!ModelState.IsValid)
@@ -46,5 +61,6 @@
             }
             return Json(input);
         }
+
     }
 }
