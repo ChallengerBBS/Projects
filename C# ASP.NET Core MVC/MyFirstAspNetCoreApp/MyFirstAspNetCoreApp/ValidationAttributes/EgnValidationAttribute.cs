b564b83a8f44307a932ba0dd4ade07f432771878
Sevdalin Vasilev <predator187@abv.bg>
2020-05-27 13:03:58 +0300

    Added EGN validation attribute

---
+++ C# ASP.NET Core MVC/MyFirstAspNetCoreApp/MyFirstAspNetCoreApp/ValidationAttributes/EgnValidationAttribute.cs
@@ -0,0 +1,36 @@
+﻿namespace MyFirstAspNetCoreApp.ValidationAttributes
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Text.RegularExpressions;
+
+    public class EgnValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var valueAsString = value.ToString();
+
+            if (!Regex.IsMatch(valueAsString, "[0-9]{10}"))
+            {
+                return new ValidationResult("ЕГН-то трябва да съдържа 10 чифри !");
+            }
+            var weight = new[] { 2, 4, 8, 5, 10, 9, 7, 3, 6 };
+            int checksum = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                checksum+=(valueAsString[i] - '0') * weight[i];
+            }
+            var lastDigit = checksum % 11;
+            if (lastDigit == 10)
+            {
+                lastDigit = 0;
+            }
+            if(valueAsString[9]-'0'!=lastDigit)
+            {
+                return new ValidationResult("Невалидно ЕГН !");
+
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
