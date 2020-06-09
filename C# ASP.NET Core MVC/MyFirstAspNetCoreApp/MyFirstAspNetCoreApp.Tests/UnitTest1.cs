488d310711e4a3b39d87e9a18e15029aca62b6c3
Sevdalin Vasilev <predator187@abv.bg>
2020-06-09 16:57:56 +0300

    Initial commit of unit testing project

---
+++ C# ASP.NET Core MVC/MyFirstAspNetCoreApp/MyFirstAspNetCoreApp.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MyFirstAspNetCoreApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
